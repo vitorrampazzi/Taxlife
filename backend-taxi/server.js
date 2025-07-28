b3249184194080d01710e08912ecf6e935375122
vitorrampazzi <vitor.rampazzi@gmail.com>
2025-07-28 12:55:05 -0300

    Adicionando rotas ao login de usuarios e taxistas

--- backend-taxi/server.js
+++ backend-taxi/server.js
@@ -66,3 +66,41 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+
+app.post('/api/login-usuario', (req, res) => {
+  const { email, senha } = req.body;
+
+  const sql = 'SELECT * FROM users WHERE Email = ? AND Senha = ?';
+  db.query(sql, [email, senha], (err, results) => {
+    if (err) {
+      console.error('Erro ao fazer login de usuário:', err);
+      return res.status(500).json({ erro: 'Erro interno do servidor' });
+    }
+
+    if (results.length > 0) {
+      res.status(200).json({ mensagem: 'Login de usuário bem-sucedido', usuario: results[0] });
+    } else {
+      res.status(401).json({ erro: 'Email ou senha inválidos' });
+    }
+  });
+});
+
+
+app.post('/api/login-taxista', (req, res) => {
+  const { email, senha } = req.body;
+
+  const sql = 'SELECT * FROM drivers WHERE email = ? AND password = ?';
+  db.query(sql, [email, senha], (err, results) => {
+    if (err) {
+      console.error('Erro ao fazer login de taxista:', err);
+      return res.status(500).json({ erro: 'Erro interno do servidor' });
+    }
+
+    if (results.length > 0) {
+      res.status(200).json({ mensagem: 'Login de taxista bem-sucedido', taxista: results[0] });
+    } else {
+      res.status(401).json({ erro: 'Email ou senha inválidos' });
+    }
+  });
+});
